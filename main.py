b1a0ef86d1430c33bd82faae233b7e8c1f7567e4
Dinesh <TheTrufth@programmer.net>
2021-03-10 21:58:03 +0000

    Tidied up some code

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ class MainApplication(tk.Frame):
     def mainMenu(self):
         ''' Main Menu '''
         self.MenuFrame = tk.Frame(self.parent)
-        self.Label = tk.Label(self.MenuFrame, text="Select option").grid(row=0, column=1)
+        Label = tk.Label(self.MenuFrame, text="Select option").grid(row=0, column=1)
         self.CalculatorMenuLogo = tk.PhotoImage(file=r"pics/CalculatorMenuLogo.png")
         self.PracticeMenuLogo = tk.PhotoImage(file=r"pics/PracticeMenuLogo.png")
         self.LearnMenuLogo = tk.PhotoImage(file=r"pics/LearnMenuLogo.png")
-        self.CalculatorMenuButton = tk.Button(self.MenuFrame, text="Calculator", image=self.CalculatorMenuLogo, command=lambda: self.goto_x_Menu(1)).grid(row=1, column=0)
-        self.PracticeMenuButton = tk.Button(self.MenuFrame, text="Practice", image=self.PracticeMenuLogo, command=lambda: self.goto_x_Menu(2)).grid(row=1, column=1)
-        self.LearnMenuButton = tk.Button(self.MenuFrame, text="Learn", image=self.LearnMenuLogo, command=lambda: self.goto_x_Menu(3)).grid(row=1, column=2)
+        CalculatorMenuButton = tk.Button(self.MenuFrame, text="Calculator", image=self.CalculatorMenuLogo, command=lambda: self.goto_x_Menu(1)).grid(row=1, column=0)
+        PracticeMenuButton = tk.Button(self.MenuFrame, text="Practice", image=self.PracticeMenuLogo, command=lambda: self.goto_x_Menu(2)).grid(row=1, column=1)
+        LearnMenuButton = tk.Button(self.MenuFrame, text="Learn", image=self.LearnMenuLogo, command=lambda: self.goto_x_Menu(3)).grid(row=1, column=2)
+        LoadAccountButton = tk.Button(self.MenuFrame, text="LOAD ACCOUNT", command=lambda: self.goto_x_Menu(4)).grid(row=2, column=0)
         self.MenuFrame.pack()
 
     def clearwin(self):
@@ -33,25 +34,33 @@ class MainApplication(tk.Frame):
             self.practiceMenu()
         elif x == 3:
             self.learnMenu()
+        elif x == 4:
+            self.loadAccount()
         
     def calculatorMenu(self):
         self.CalculatorMenuFrame = tk.Frame(self.parent)
-        self.Label = tk.Label(self.CalculatorMenuFrame, text="Welcome to calculator menu").grid(row=0, column=0)
-        self.goBackButton = tk.Button(self.CalculatorMenuFrame, text="Go Back", command=lambda: self.goto_x_Menu(0)).grid(row=1, column=0)
+        Label = tk.Label(self.CalculatorMenuFrame, text="Welcome to calculator menu").grid(row=0, column=0)
+        goBackButton = tk.Button(self.CalculatorMenuFrame, text="Go Back", command=lambda: self.goto_x_Menu(0)).grid(row=1, column=0)
         self.CalculatorMenuFrame.pack()
     
     def practiceMenu(self):
         self.PracticeMenuFrame = tk.Frame(self.parent)
-        self.Label = tk.Label(self.PracticeMenuFrame, text="Welcome to practice menu").grid(row=0, column=0)
-        self.goBackButton = tk.Button(self.PracticeMenuFrame, text="Go Back", command=lambda: self.goto_x_Menu(0)).grid(row=1, column=0)
+        Label = tk.Label(self.PracticeMenuFrame, text="Welcome to practice menu").grid(row=0, column=0)
+        goBackButton = tk.Button(self.PracticeMenuFrame, text="Go Back", command=lambda: self.goto_x_Menu(0)).grid(row=1, column=0)
         self.PracticeMenuFrame.pack()
     
     def learnMenu(self):
         self.LearnMenuFrame = tk.Frame(self.parent)
-        self.Label = tk.Label(self.LearnMenuFrame, text="Welcome to learn menu").grid(row=0, column=0)
-        self.goBackButton = tk.Button(self.LearnMenuFrame, text="Go Back", command=lambda: self.goto_x_Menu(0)).grid(row=1, column=0)
+        Label = tk.Label(self.LearnMenuFrame, text="Welcome to learn menu").grid(row=0, column=0)
+        goBackButton = tk.Button(self.LearnMenuFrame, text="Go Back", command=lambda: self.goto_x_Menu(0)).grid(row=1, column=0)
         self.LearnMenuFrame.pack()
-        
+    
+    def loadAccount(self):
+        self.LoadAccountFrame = tk.Frame(self.parent)
+        Label = tk.Label(self.LoadAccountFrame, text="Here you can load your account and get back where you left off").grid(row=0, column=0)
+        goBackButton = tk.Button(self.LoadAccountFrame, text="Go Back", command=lambda: self.goto_x_Menu(0)).grid(row=1, column=0)
+        self.LoadAccountFrame.pack()
+
 
 
 if __name__ == "__main__":
