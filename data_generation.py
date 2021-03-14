0a910aad2d7d13728e4d216f64655bc6fea4ba31
Nathan <67518634+zcabnds@users.noreply.github.com>
2021-03-14 16:21:19 +0000

    Add files via upload

--- data_generation.py
+++ data_generation.py
@@ -1,33 +1,22 @@
 import random
+#from sympy.abc import A, B, C
+from sympy.logic.boolalg import to_dnf
+from sympy.logic.boolalg import Or
+from sympy.logic.boolalg import And
+from sympy import Symbol 
+
 def get_prop_formula():
-        
-    points = []
-    numberOfCharacters = random.randint(1,4)
-    characters = []
-
-    for i in range(0,numberOfCharacters):
-        NOT = random.randint(0,1)
-        R = random.randint(65,90)
-        if NOT == 0:
-            characters.append(u'¬' + chr(R))
-        else:
-            characters.append(chr(R))
-
-    symbols = []
-    symbols.append(u'∧')
-    symbols.append(u'→')
-    symbols.append(u'∨')
-
+    characters = ['A','B','C']
+    operators = ['&','|']
     sentence = ''
-    if len(characters) == 1:
-        sentence = characters[0]
-    else:
-        for i in range(0,len(characters)):
-            if i == len(characters) - 1:
-                sentence += characters[i]
-            else:
-                sentence += characters[i] + symbols[random.randint(0,len(symbols)-1)]
-    
+    for i in range(0,4):
+        if i == 3:
+            sentence += characters[random.randint(0,2)]
+        else:
+            sentence += characters[random.randint(0,2)] + operators[random.randint(0,1)]
     print(sentence)
+    return sentence
 
-get_prop_formula()
+def convertToDNF():
+    print(to_dnf(get_prop_formula()))
+convertToDNF()
