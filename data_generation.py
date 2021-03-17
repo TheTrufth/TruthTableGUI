0dd3c60ef3e1d1ed69c493011a23dd97caa36958
Nathan <67518634+zcabnds@users.noreply.github.com>
2021-03-17 10:45:23 +0000

    Add files via upload

--- data_generation.py
+++ data_generation.py
@@ -1,26 +1,34 @@
 import random
-#from sympy.abc import A, B, C
 from sympy.logic.boolalg import to_dnf, to_cnf
 from sympy.logic.boolalg import Or
 from sympy.logic.boolalg import And
+from sympy.logic.boolalg import is_dnf , is_cnf
 from sympy import Symbol 
 
+#function to make random propositional formulas
 def get_prop_formula():
-    characters = ['A','B','C']
+    characters = ['A','B','C','~A','~B','~C']
     operators = ['&','|']
     sentence = ''
     for i in range(0,4):
         if i == 3:
             sentence += characters[random.randint(0,2)]
         else:
-            sentence += characters[random.randint(0,2)] + operators[random.randint(0,1)]
-    print(sentence)
+            sentence += characters[random.randint(0,5)] + operators[random.randint(0,1)]
     return sentence
+#function to be used in calculator to convert propositional formula into DNF
+def convertToDNF(formula):
+    return to_dnf(get_prop_formula())
 
-def convertToDNF():
-    print("DNF = " , to_dnf(get_prop_formula()))
+#function to be used in calculator to convert propositional formula into CNF
+def convertToCNF(formula):
+    return to_cnf(get_prop_formula())
 
-def convertToCNF():
-    print("CNF = " ,to_cnf(get_prop_formula()))
-convertToDNF()
-convertToCNF()
+#function to be used in practice part of the program to validate when user has to convert formula into CNF
+def check_if_cnf(formula):
+    return is_cnf(formula)
+    
+#function to be used in practice part of the program to validate when user has to convert formula into CNF
+def check_if_dnf(formula):
+    return is_cnf(formula)
+    
